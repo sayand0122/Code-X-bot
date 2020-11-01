bd5a2c967dcbdb466739b6ca00ce4d9ae5449a10
sayand0122 <sayandutta.jan@gmail.com>
2020-11-01 14:07:02 +0530

    Minor Fixes

--- bot.py
+++ bot.py
@@ -12,10 +12,8 @@ load_dotenv()
 client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 
-status = cycle(['Bored in the House','Listening to Bad Guy by Billie Eilish','Type commands to see how I function😁','Working on my own code','Stay Safe','College life is Chill'])
-
-
-
+status = cycle(['Bored in the House', 'Listening to Bad Guy by Billie Eilish',
+                'Type commands to see how I function😁', 'Working on my own code', 'Stay Safe', 'College life is Chill'])
 
 
 # its a function decorator that denotes the function is going to represent an event
@@ -27,17 +25,10 @@ async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
 
-
-
-
-
 # changes the game activity of bot every 30 mins
 @tasks.loop(seconds=30)
 async def change_status():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(next(status)))
-
-
-
 
 
 # errors handling
@@ -47,19 +38,17 @@ async def on_command_error(ctx, error):
         await ctx.send('```Inavlid Command used.```')
 
 
-
 # clear commands and its error handling
 @client.command()
 @commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount : int):
-    await ctx.channel.purge(limit = amount)
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
+
+
 @clear.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please pass the amount to clear the number of data.")
-
-
-
 
 
 # ping it and it returns your latency
@@ -68,18 +57,17 @@ async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
 
-
-
 # links to various sites
 @client.command(aliases=['media'])
 async def links(ctx):
-    myembed = discord.Embed(title='Links to our Social sites' , description='' , color=discord.Colour.red())
-    myembed.add_field(name='Follow us at Instagram' , value='*[Instagram](https://www.instagram.com/codex_gitam "Link to our insta page")*' , inline=False)
-    myembed.add_field(name='View our Github Page' , value='*[GitHub](https://github.com/c-code-x "Link to ou github organization")*' , inline=False)
-    myembed.add_field(name='Our Website' , value='*[Website](https://google.com "Link to our Code-X website")*' , inline=False)
+    myembed = discord.Embed(title='Links to our Social sites',
+                            description='', color=discord.Colour.red())
+    myembed.add_field(name='Follow us at Instagram',
+                      value='*[Instagram](https://www.instagram.com/codex_gitam "Link to our insta page")*', inline=False)
+    myembed.add_field(name='View our Github Page',
+                      value='*[GitHub](https://github.com/c-code-x "Link to ou github organization")*', inline=False)
+    myembed.add_field(name='Our Website', value='[Website]', inline=False)
     await ctx.send(embed=myembed)
-
-
 
 
 # help command
@@ -88,19 +76,18 @@ async def help(ctx):
     await ctx.send("```Note - Type the prefix '.' before every command```\n```List of commands\n• .help/.assist\n• .ping \n• .links \n• .whois 'exact core-member name' \n• .clear 'specify amount here' \n• .courses \n• .courses 'exact domain name'```")
 
 
-
 # maintaining cogs
 @client.command()
 async def load(ctx, extension):
     client.load_exntension(f'cogs.{extension}')
+
+
 @client.command()
 async def unload(ctx, extension):
     client.unload_exntension(f'cogs.{extension}')
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
-
 
 
 # after we gave all the info to the bot we need to run this client (in brakets is the unique bot token)
--- cogs/coreteam.py
+++ cogs/coreteam.py
@@ -3,89 +3,118 @@ from discord.ext import commands
 from discord.guild import Guild
 from discord import role
 
+
 class coreteam(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-    
-    #Events
+
+    # Events
     @commands.Cog.listener()
     async def on_ready(self):
         print("CoreTeam cog is ready")
-    
 
-    #Commands
+    # Commands
+
     @commands.command()
-    async def whois(self, ctx, member : discord.Member):
+    async def whois(self, ctx, member: discord.Member):
         if member.nick == "Sayan":
-            e = discord.Embed(title = member.nick , description = 'Heya guys nice to meet you all. I am Sayan and we can have lots of fun together' , color = 0x140ec7)
-            e.add_field(name='My projects bundle',value='**[GitHub](https://github.com/sayand0122)**',inline=False)
-            e.add_field(name='Socialization spot',value='**[LinkedIn](https://www.linkedin.com/in/sayan-dutta-117a8a1a8/)**',inline=False)
+            e = discord.Embed(
+                title=member.nick, description='Heya guys nice to meet you all. I am Sayan and we can have lots of fun together', color=0x140ec7)
+            e.add_field(name='My projects bundle',
+                        value='**[GitHub](https://github.com/sayand0122)**', inline=False)
+            e.add_field(name='Socialization spot',
+                        value='**[LinkedIn](https://www.linkedin.com/in/sayan-dutta-117a8a1a8/)**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
-            e.set_footer(icon_url=ctx.author.avatar_url, text=f'Requested by {ctx.author.name}')
+            e.set_footer(icon_url=ctx.author.avatar_url,
+                         text=f'Requested by {ctx.author.name}')
             await ctx.send(embed=e)
-        
-        elif member.name=="Loganwick":
-            e = discord.Embed(title = member.name , description = 'Hey guys Jai this side U can call me logan😁' , color = 0x1cc70c)
-            e.add_field(name='You can follow me up on github',value='**[Here](https://github.com/Jaideep-C "Explore my projects here")**',inline=False)
-            e.add_field(name='Wanna connect with me on LinkedIn',value='**[Here](https://www.linkedin.com/in/jaideep-c-807413138/ "Connect with me here")**',inline=False)
+
+        elif member.name == "Loganwick":
+            e = discord.Embed(
+                title=member.name, description='Hey guys Jai this side U can call me logan😁', color=0x1cc70c)
+            e.add_field(name='You can follow me up on github',
+                        value='**[Here](https://github.com/Jaideep-C "Explore my projects here")**', inline=False)
+            e.add_field(name='Wanna connect with me on LinkedIn',
+                        value='**[Here](https://www.linkedin.com/in/jaideep-c-807413138/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
 
-        elif member.name=="srinijads":
-            e = discord.Embed(title = member.name , description = "I'm Srinija and I like to code." , color = 0xf0e118)
-            e.add_field(name='Way to my github',value='**[Here](https://github.com/srinijadharani "Explore my projects")**',inline=False)
-            e.add_field(name='LinkedIn',value='**[Here](https://www.linkedin.com/in/srinijadharani/ "Connect with me")**',inline=False)
+        elif member.name == "srinija":
+            e = discord.Embed(
+                title=member.name, description="I'm Srinija and I like to code.", color=0xf0e118)
+            e.add_field(name='Way to my github',
+                        value='**[Here](https://github.com/srinijadharani "Explore my projects")**', inline=False)
+            e.add_field(
+                name='LinkedIn', value='**[Here](https://www.linkedin.com/in/srinijadharani/ "Connect with me")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
 
-        elif member.name=="NeerajAP":
-            e = discord.Embed(title = member.name , description = "Hey guys Neeraj this side." , color = 0xf0e118)
-            e.add_field(name='My projects',value='**[Here](https://github.com/NEERAJAP2001 "Explore my projects here")**',inline=False)
-            e.add_field(name='LinkedIn',value='[Here](https://www.linkedin.com/in/neeraj-adityananth/ "Connect with me here")**',inline=False)
-            e.set_thumbnail(url=member.avatar_url)
-            await ctx.send(embed=e)
-            
-        elif member.name=="bharxhav":
-            e = discord.Embed(title = member.name , description = "Hi Bhargav here." , color = 0x1cc70c)
-            e.add_field(name='You can follow me up on github',value='**[Here](https://github.com/bharxhav "Explore my projects here")**',inline=False)
-            e.add_field(name='Wanna connect with me on LinkedIn',value='**[Here](https://www.linkedin.com/in/bhargavkantheti/ "Connect with me here")**',inline=False)
+        elif member.name == "NeerajAP":
+            e = discord.Embed(
+                title=member.name, description="Hey guys Neeraj this side.", color=0xf0e118)
+            e.add_field(
+                name='My projects', value='**[Here](https://github.com/NEERAJAP2001 "Explore my projects here")**', inline=False)
+            e.add_field(
+                name='LinkedIn', value='[Here](https://www.linkedin.com/in/neeraj-adityananth/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
 
-        elif member.name=="Navya":
-            e = discord.Embed(title = member.name , description = " Yeoboseyo I'm Navya let's work together and learn together" , color = 0xdb142b)
-            e.add_field(name='You can follow me up on github',value='**[Here](https://github.com/navya-005 "Explore my projects here")**',inline=False)
-            e.add_field(name='Wanna connect with me on LinkedIn',value='**[Here](https://www.linkedin.com/in/navyasree-mallela-b0a4931ba/ "Connect with me here")**',inline=False)
+        elif member.name == "bharxhav":
+            e = discord.Embed(title=member.name,
+                              description="Hi Bhargav here.", color=0x1cc70c)
+            e.add_field(name='You can follow me up on github',
+                        value='**[Here](https://github.com/bharxhav "Explore my projects here")**', inline=False)
+            e.add_field(name='Wanna connect with me on LinkedIn',
+                        value='**[Here](https://www.linkedin.com/in/bhargavkantheti/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
 
-        elif member.name=="Madhulika":
-            e = discord.Embed(title = member.name , description = "I'm Madhulika" , color = 0xdb142b)
-            e.add_field(name='You can follow me up on github',value='**[Here](https://github.com/madhulika01 "Explore my projects here")**',inline=False)
-            e.add_field(name='Wanna connect with me on LinkedIn',value='**[Here](https://www.linkedin.com/in/madhulika-akumalla-a984ab1ba/ "Connect with me here")**',inline=False)
-            e.set_thumbnail(url=member.avatar_url)
-            await ctx.send(embed=e)
-           
-        elif member.name=="Dark Quill":
-            e = discord.Embed(title = member.name , description = "Hi guys nice to meet you all. I am Nishitha" , color = 0xdb6714)
-            e.add_field(name='Link to my LinkedIn',value='**[Click Here](https://www.linkedin.com/in/nishitha-motakatla-187465193/ "Connect with me here")**',inline=False)
+        elif member.name == "Navya":
+            e = discord.Embed(
+                title=member.name, description=" Yeoboseyo I'm Navya let's work together and learn together", color=0xdb142b)
+            e.add_field(name='You can follow me up on github',
+                        value='**[Here](https://github.com/navya-005 "Explore my projects here")**', inline=False)
+            e.add_field(name='Wanna connect with me on LinkedIn',
+                        value='**[Here](https://www.linkedin.com/in/navyasree-mallela-b0a4931ba/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
 
-        elif member.name=="Manoj":
-            e = discord.Embed(title = member.name , description = "Hi guys! Great to see you all. I am Manoj" , color = 0x1156a6)
-            e.add_field(name='My LinkedIn',value='**[Click Here](https://www.linkedin.com/in/k-manoj-manu-4224941ba "Connect with me here")**',inline=False)
+        elif member.name == "Madhulika":
+            e = discord.Embed(title=member.name,
+                              description="I'm Madhulika", color=0xdb142b)
+            e.add_field(name='You can follow me up on github',
+                        value='**[Here](https://github.com/madhulika01 "Explore my projects here")**', inline=False)
+            e.add_field(name='Wanna connect with me on LinkedIn',
+                        value='**[Here](https://www.linkedin.com/in/madhulika-akumalla-a984ab1ba/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
-        
-        elif member.name=="Nanda":
-            e = discord.Embed(title = member.name , description = "A self-taught web developer, gearing toward the field of AI/ML. A passionate coder who loves exploring every aspect of tech." , color = 0xdb142b)
-            e.add_field(name='My Github',value='**[Here](https://github.com/nandaraj2001 "Explore my projects here")**',inline=False)
-            e.add_field(name='My LinkedIn',value='**[Here](https://www.linkedin.com/in/nanda-rajesh/ "Connect with me here")**',inline=False)
+
+        elif member.name == "Dark Quill":
+            e = discord.Embed(
+                title=member.name, description="Hi guys nice to meet you all. I am Nishitha", color=0xdb6714)
+            e.add_field(name='Link to my LinkedIn',
+                        value='**[Click Here](https://www.linkedin.com/in/nishitha-motakatla-187465193/ "Connect with me here")**', inline=False)
             e.set_thumbnail(url=member.avatar_url)
             await ctx.send(embed=e)
-    
+
+        elif member.nick == "Manoj":
+            e = discord.Embed(
+                title=member.name, description="Hi guys! Great to see you all. I am Manoj", color=0x1156a6)
+            e.add_field(
+                name='My LinkedIn', value='**[Click Here](https://www.linkedin.com/in/k-manoj-manu-4224941ba "Connect with me here")**', inline=False)
+            e.set_thumbnail(url=member.avatar_url)
+            await ctx.send(embed=e)
+
+        elif member.name == "Nanda":
+            e = discord.Embed(
+                title=member.name, description="A self-taught web developer, gearing toward the field of AI/ML. A passionate coder who loves exploring every aspect of tech.", color=0xdb142b)
+            e.add_field(
+                name='My Github', value='**[Here](https://github.com/nandaraj2001 "Explore my projects here")**', inline=False)
+            e.add_field(
+                name='My LinkedIn', value='**[Here](https://www.linkedin.com/in/nanda-rajesh/ "Connect with me here")**', inline=False)
+            e.set_thumbnail(url=member.avatar_url)
+            await ctx.send(embed=e)
 
     @whois.error
     async def whois_error(self, ctx, error):
